498618acc634f2f7f7c78abbf872336699c9e49c
Nazar <nazarijankosuk@gmail.com>
2025-06-13 14:33:17 +0300

    second hw changes

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,10 +3,19 @@ import contactsSlice from "./contactsSlice.js";
 import filtersSlice from "./filtersSlice.js";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import {
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
 
 const persistConfig = {
   key: "contacts",
   storage,
+  whitelist: ["items"],
 };
 
 const persistContactsReducer = persistReducer(persistConfig, contactsSlice);
@@ -16,6 +25,12 @@ export const store = configureStore({
     contacts: persistContactsReducer,
     filters: filtersSlice,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
-export const persist = persistStore(store);
+export const persistor = persistStore(store);
